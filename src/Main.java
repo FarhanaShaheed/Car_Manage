902796d107626ac3980f426eca4580f2684c61c4
FarhanaShaheed <farhana15-734@diu.edu.bd>
2024-09-28 13:13:04 +0200

    update code

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,12 @@ public class Main {
     public static void main(String[] args) {
         CarManager manager = new CarManager();
         try {
-            manager.saveCarsByBrand("Toyota", "toyota_cars.txt");
+            manager.saveCarsByBrand("Toyota", "cars_by_brand.txt");
             System.out.println("Toyota cars saved.");
-            manager.saveCarsByModelAndAge("Civic", 5, "old_civics.txt");
+            manager.saveCarsByModelAndAge("Civic", 5, "cars_by_modelyears.txt");
             System.out.println("Old Civics saved.");
-            manager.saveCarsByYearAndPrice(2015, 12000, "expensive_2015_cars.txt");
-            System.out.println("Expensive 2015 cars saved.");
+            manager.saveCarsByYearAndPrice(2015, 12000, "cars_by_year_and_price.txt");
+            System.out.println("Expensive cars saved.");
         } catch (IOException e) {
             System.out.println("An error occurred:");
             e.printStackTrace();
